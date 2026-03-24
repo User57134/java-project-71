49f162bdd565005ab4e8d533e0cbabc4835eeb95
Player One <pl17dev@gmail.com>
2026-03-24 13:10:52 +0300

    step 0

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.53.0"
+    id("java")
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
