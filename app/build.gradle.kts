e292c8b09aa87d63fb38d623d20e3f8ada4fd499
Player One <pl17dev@gmail.com>
2026-03-25 16:23:42 +0300

    step 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,8 @@ application {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.7")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,46 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Compares two configuration files and shows a difference.")
+class Differ implements Callable<Integer> {
+
+//    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+//    private File file;
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private boolean help = false;
+
+    @Option(names = { "-V", "--version" }, description = "Print version information and exit.")
+    private boolean version = false;
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+//        byte[] fileContents = Files.readAllBytes(file.toPath());
+//        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
+//        System.out.printf("%0" + (digest.length * 2) + "x%n", new BigInteger(1, digest));
+        return 0;
+    }
+}
+
+
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+
+        int exitCode = new CommandLine(new Differ()).execute(args);
+        System.exit(exitCode);
     }
 }
