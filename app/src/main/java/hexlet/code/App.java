6304861f002dbec3124c9be1ca9c4f6b055b88b6
Player One <pl17dev@gmail.com>
2026-03-28 11:13:50 +0300

    step 4.1, parameters filepath1 and filepath2 was added

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,29 +6,36 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Compares two configuration files and shows a difference.")
 class Differ implements Callable<Integer> {
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private File filepath1;
 
-    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private File filepath2;
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit")
     private boolean help = false;
 
-    @Option(names = { "-V", "--version" }, description = "Print version information and exit.")
+    @Option(names = { "-V", "--version" }, description = "Print version information and exit")
     private boolean version = false;
 
-    @Option(names = {"-f", "--format"}, paramLabel = "FORMAT", description = "output format [default: stylish].")
+    @Option(names = {"-f", "--format"}, paramLabel = "FORMAT", description = "output format [default: stylish]")
     private String format = "stylish ";
 
     @Override
     public Integer call() throws Exception { // your business logic goes here...
-//        byte[] fileContents = Files.readAllBytes(file.toPath());
-//        byte[] digest = MessageDigest.getInstance(algorithm).digest(fileContents);
-//        System.out.printf("%0" + (digest.length * 2) + "x%n", new BigInteger(1, digest));
+
         return 0;
     }
 }
@@ -38,7 +45,6 @@ class Differ implements Callable<Integer> {
 public class App {
 
     public static void main(String[] args) {
-        //System.out.println("Hello World!");
 
         int exitCode = new CommandLine(new Differ()).execute(args);
         System.exit(exitCode);
