c0d09f8cdfb43fc88c1144be77165207b92b6582
Player One <pl17dev@gmail.com>
2026-04-07 16:18:54 +0300

    step 11, try 3

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -52,7 +52,7 @@ final class Parser {
 
 @CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
         description = "Compares two configuration files and shows a difference.")
-public class Differ implements Callable<Integer> {
+public final class Differ implements Callable<Integer> {
 
 
     static String getFileExtension(String filename) {
