15dacffec6783bcf1ca2e1d260bbefc85792d4bd
Player One <pl17dev@gmail.com>
2026-04-13 23:30:32 +0300

    step 11: tutor s remarks fixing, fix 4

---
+++ app/src/test/java/hexlet/code/FormatterTest.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FormatterTest {
+
+    @Test
+    void getFormatterTest() throws Exception {
+        var formatter = Formatters.getFormatter("stylish");
+        assertEquals("hexlet.code.formatters.StylishFormatter", formatter.getClass().getName());
+
+        formatter = Formatters.getFormatter("plain");
+        assertEquals("hexlet.code.formatters.PlainFormatter", formatter.getClass().getName());
+
+        formatter = Formatters.getFormatter("json");
+        assertEquals("hexlet.code.formatters.JsonFormatter", formatter.getClass().getName());
+
+        formatter = null;
+        try {
+            formatter = Formatters.getFormatter("other");
+        } catch (Exception ex) {
+            // nothing
+        }
+        assertEquals(null, formatter);
+
+    }
+}
